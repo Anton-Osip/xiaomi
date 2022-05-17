ff39f79f849e35e4d3406d8c4c286510a8225146
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-05-17 21:41:40 +0300

    finish withaut media

--- js/script.js
+++ js/script.js
@@ -7,4 +7,12 @@ $(function () {
 		autoplay: true,
 		autoplaySpeed: 2000,
 	})
+	$('.slider__items').slick({
+		arrows: false,
+		dots: true,
+		fade: true,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	})
 })
